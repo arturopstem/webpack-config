8053563f67ae9d408435c0e1ffabfa3650055031
arturop <arturopstem@gmail.com>
2023-08-04 19:08:17 -0500

    Webpack config + ESLint + Prettier

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './main.css';
+
+const markup = `
+<h1>Webpack + ESLint + Prettier</h1>
+`;
+
+const h1 = new DOMParser()
+  .parseFromString(markup, 'text/html')
+  .querySelector('h1');
+
+document.body.appendChild(h1);
