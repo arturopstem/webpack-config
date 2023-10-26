0de21ad18626951c83d9e3e1365d9a83241e86bc
arturop <67555134+arturopstem@users.noreply.github.com>
2023-10-26 02:01:26 -0500

    Sass + Babel + ESLint + Prettier

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const config = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new ESLintPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+
+  output: {
+    clean: true,
+  },
+
+  devServer: { watchFiles: './src/**/*' },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
+  if (argv.mode === 'production') {
+    config.optimization = {
+      minimizer: [new CssMinimizerPlugin(), '...'],
+    };
+  }
+  return config;
+};
